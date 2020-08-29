e39d06ad8258273b073f36750cf3331164977883
Mike Gellman <mikegellman@Mikes-MacBook-Pro-2.local>
2020-08-29 01:41:31 -0500

    added new/create/destroy methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles = Article.all 
     end
 
     def show
-        @articles = Article.find(params[:id])
+        @articles = Article.find(params[:id]) 
     end
+
+    def new
+        @articles = Article.new 
+    end
+
+    def create
+        @articles = Article.new(article_params)
+        @articles.save
+
+        redirect_to articles_path(@articles)
+    end
+
+    def destroy
+        @articles = Article.find(params[:id]) 
+        @articles.destroy
+        
+        redirect_to articles_path
+    end
+
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+    
+    def article_params
+        params.require(:article).permit(:id, :title, :body)
+    end
+
 end
